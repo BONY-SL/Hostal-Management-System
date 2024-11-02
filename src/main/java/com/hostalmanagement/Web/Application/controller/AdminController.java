c343b04f60d47569ee7d3e092ee1792dd6ddd634
DILSHAN PATHIRAGE <danidutharuka12345@gmail.com>
2024-11-02 15:02:48 +0530

    update

--- src/main/java/com/hostalmanagement/Web/Application/controller/AdminController.java
+++ src/main/java/com/hostalmanagement/Web/Application/controller/AdminController.java
@@ -1,4 +1,11 @@
 package com.hostalmanagement.Web.Application.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@CrossOrigin("*")
+@RequestMapping("hostalmanage")
+@RequiredArgsConstructor
 public class AdminController {
 }
