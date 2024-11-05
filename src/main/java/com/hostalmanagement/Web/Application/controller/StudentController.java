0fbc7517e4c923cd66c1fee9c58aa90c5a3e88f3
DILSHAN PATHIRAGE <danidutharuka12345@gmail.com>
2024-11-05 14:58:57 +0530

    update

--- src/main/java/com/hostalmanagement/Web/Application/controller/StudentController.java
+++ src/main/java/com/hostalmanagement/Web/Application/controller/StudentController.java
@@ -3,6 +3,7 @@ import com.hostalmanagement.Web.Application.dto.StudentDto;
 import com.hostalmanagement.Web.Application.service.StudentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @Controller
-@RequestMapping("/hostel")
+@RequestMapping("/hostalmanage/student")
+@PreAuthorize("hasAuthority('STUDENT')")
 @RequiredArgsConstructor
 public class StudentController {
     private final StudentService studentService;
